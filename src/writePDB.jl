4c975cbdbda18ab1e7a9ff6c3f3a3eeb8f137176
Leandro Martinez <leandro@iqm.unicamp.br>
2020-02-17 16:31:59 -0300

    added sel option to writePDB

--- src/writePDB.jl
+++ src/writePDB.jl
@@ -1,12 +1,19 @@
 #
-# Function that writes a PDB file given the vector of atoms
+# Function that writes a PDB file given the vector of atoms, with
+# optional definition of a selection to be print
 #
 
-function writePDB( atoms :: Union{Vector{Atom},Vector{MutableAtom}}, filename )
+function writePDB( atoms :: Union{Vector{Atom},Vector{MutableAtom}}, filename; sel = Nothing )
 
   file = open(filename,"w")
-  for atom in atoms
-    println(file,write_atom(atom))
+  if sel == Nothing
+    for atom in atoms
+      println(file,write_atom(atom))
+    end
+  else
+    for i in 1:length(sel)
+      println(file,write_atom(atom[sel[i]]))
+    end
   end
   close(file)
 
