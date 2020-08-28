990e9b64463f404335e80b404ae806b2cdd30651
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-28 09:28:09 -0300

    updated tests

--- test/select.jl
+++ test/select.jl
@@ -1,45 +1,45 @@
 @testset "Selections" begin
 
-  atoms = PDBTools.readPDB("./structure.pdb")
+  atoms = readPDB("./structure.pdb")
   
-  @test length(PDBTools.select(atoms,"name CA")) == 104
-  sel = PDBTools.select(atoms,"index = 13") 
+  @test length(select(atoms,"name CA")) == 104
+  sel = select(atoms,"index = 13") 
   @test length(sel) == 1
   @test sel[1].index == 13
   @test sel[1].index_pdb == 13
 
-  @test length(PDBTools.select(atoms,"index > 1 and index < 13")) == 11
+  @test length(select(atoms,"index > 1 and index < 13")) == 11
 
-  @test length(PDBTools.select(atoms,"protein")) == 1463
+  @test length(select(atoms,"protein")) == 1463
 
-  @test length(PDBTools.select(atoms,"water")) == 58014
+  @test length(select(atoms,"water")) == 58014
    
-  @test length(PDBTools.select(atoms,"resname GLY")) == 84
+  @test length(select(atoms,"resname GLY")) == 84
 
-  @test length(PDBTools.select(atoms,"segname PROT")) == 1463
+  @test length(select(atoms,"segname PROT")) == 1463
 
-  @test length(PDBTools.select(atoms,"residue = 2")) == 11
+  @test length(select(atoms,"residue = 2")) == 11
 
-  @test length(PDBTools.select(atoms,"neutral")) == 1182
+  @test length(select(atoms,"neutral")) == 1182
 
-  @test length(PDBTools.select(atoms,"charged")) == 281
+  @test length(select(atoms,"charged")) == 281
 
-  @test length(PDBTools.select(atoms,"sidechain")) == 854
+  @test length(select(atoms,"sidechain")) == 854
 
-  @test length(PDBTools.select(atoms,"acidic")) == 162
+  @test length(select(atoms,"acidic")) == 162
 
-  @test length(PDBTools.select(atoms,"basic")) == 68
+  @test length(select(atoms,"basic")) == 68
 
-  @test length(PDBTools.select(atoms,"hydrophobic")) == 327
+  @test length(select(atoms,"hydrophobic")) == 327
 
-  @test length(PDBTools.select(atoms,"hydrophobic")) == 327
+  @test length(select(atoms,"hydrophobic")) == 327
 
-  @test length(PDBTools.select(atoms,"aliphatic")) == 379
+  @test length(select(atoms,"aliphatic")) == 379
 
-  @test length(PDBTools.select(atoms,"aromatic")) == 344
+  @test length(select(atoms,"aromatic")) == 344
 
-  @test length(PDBTools.select(atoms,"polar")) == 880
+  @test length(select(atoms,"polar")) == 880
 
-  @test length(PDBTools.select(atoms,"nonpolar")) == 583
+  @test length(select(atoms,"nonpolar")) == 583
 
 end
