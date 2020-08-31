0206b80086cf568f4d1f76bec4fa165ec6b3020e
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-31 16:15:55 -0300

    changed eval to macro in Atom

--- src/Atom.jl
+++ src/Atom.jl
@@ -1,32 +1,35 @@
 
-AtomData = quote
-  index :: Int64 # The sequential index of the atoms in the file
-  index_pdb :: Int64 # The index as written in the PDB file (might be anything)
-  name :: String
-  resname :: String
-  chain :: String
-  resnum :: Int64 # Number of residue as written in PDB file
-  residue :: Int64 # Sequential residue (molecule) number in file
-  x :: Float64
-  y :: Float64
-  z :: Float64
-  b :: Float64
-  occup :: Float64
-  model :: Int64
-  segname :: String # Segment name (cols 73:76)
+macro SharedAtomData() 
+  ex = quote
+    index :: Int64 # The sequential index of the atoms in the file
+    index_pdb :: Int64 # The index as written in the PDB file (might be anything)
+    name :: String
+    resname :: String
+    chain :: String
+    resnum :: Int64 # Number of residue as written in PDB file
+    residue :: Int64 # Sequential residue (molecule) number in file
+    x :: Float64
+    y :: Float64
+    z :: Float64
+    b :: Float64
+    occup :: Float64
+    model :: Int64
+    segname :: String # Segment name (cols 73:76)
+  end
+  esc(ex)
 end
 
 # Mutable structure used to read data only
 
-@eval mutable struct MutableAtom
-  $AtomData
+mutable struct MutableAtom
+  @SharedAtomData()
 end
 MutableAtom() = empty_struct(MutableAtom)
 
 # Immutable structure used for critical code
 
-@eval struct Atom
-  $AtomData
+struct Atom
+  @SharedAtomData()
 end
 
 # Initialize mutable from immutable and vice-versa
