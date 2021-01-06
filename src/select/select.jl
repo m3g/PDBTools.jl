57083b4a409d4adb3b7eaab2e22f58f9ef8e71d9
Leandro Martinez <leandro@iqm.unicamp.br>
2021-01-06 17:45:23 -0300

    many changes in select

--- src/select/select.jl
+++ src/select/select.jl
@@ -3,124 +3,163 @@
 # analysis
 #
 
+using Parameters
+
 # Main function: receives the atoms vector and a julia function to select
 
-function select( atoms :: Vector{Atom}; by=all)
-  selected_atoms = Atom[]
-  for atom in atoms
-    if by(atom)
-      push!(selected_atoms,atom)
+function select(set::AbstractVector{T}; by=all) where T
+  selected = T[]
+  for el in set
+    if by(el)
+      push!(selected,el)
     end
   end
-  return selected_atoms
+  return selected
 end
 
 # Given a selection string
 
-function select( atoms :: Vector{Atom}, selection :: String )
+function select(set::AbstractVector{T}, selection::String) where T
   query = parse_query(selection)
-  return select(atoms, by = atom -> apply_query(query,atom))
+  return select(set, by = el -> apply_query(query,el))
 end
 
 #
 # Return indexes only
 #
-function selindex( atoms :: Vector{Atom}; by=all)
+function selindex(set::AbstractVector{T}; by=all) where T
   indexes = Vector{Int64}(undef,0)
-  for atom in atoms
-    if by(atom)
-      push!(indexes,atom.index)
+  for i in eachindex(set)
+    if by(set[i])
+      push!(indexes,i)
     end
   end
   return indexes
 end
 
-function selindex( atoms :: Vector{Atom}, selection :: String )
+function selindex(set::AbstractVector{T}, selection::String) where T
   query = parse_query(selection)
-  return selindex(atoms, by = atom -> apply_query(query,atom))
+  return selindex(set, by = el -> apply_query(query,el))
 end
 
 # Comparison operators
 
-const operators = ( " = " =>  (x,y) -> isequal(x,y),
-                    " < " =>  (x,y) -> isless(x,y),
-                    " > " =>  (x,y) -> isless(y,x), 
-                    " <= " => (x,y) -> (! isless(y,x)),
-                    " >= " => (x,y) -> (! isless(x,y)) ) 
-
-using Parameters
+const operators = ( "=" => (x,y) -> isequal(x,y),
+                    "<" => (x,y) -> isless(x,y),
+                    ">" => (x,y) -> isless(y,x), 
+                    "<=" => (x,y) -> (! isless(y,x)),
+                    ">=" => (x,y) -> (! isless(x,y)) ) 
 
 #
-# Numerical syntax keywords
+# Keywords
 #
 
-struct NumericalKeyword
+struct Keyword
+  ValueType :: Type
   name :: String
-  symbol :: Symbol
-  operations :: Tuple
+  field :: Symbol
+  operators :: Tuple
 end
 
-function parse_keyword(key::NumericalKeyword,str,op) 
-  return parse(Int, match(key.name*op*r"([0-9]*)", str)[1])   
-end
-
-#
-# String syntax keywords
-#
-
-struct StringKeyword
-  name :: String
-  symbol :: Symbol
-  operations :: Tuple
-end
-
-function parse_keyword(key::StringKeyword,str,op) 
-  return match(key.name*op*r"([A-Z,0-9]*)", str)[1] 
-end
-
-#
-# Function that returns the anonymous functions that evaluates if a condition
-# is satisfied or not for a given atom, given numerical or string keywords
-#
-
-function getfunc(key::T, str) where T <: Union{NumericalKeyword,StringKeyword}
-  @unpack name, operations, symbol = key
-  for op in operations
-    if occursin(name*op.first, str)
-      val = parse_keyword(key,str,op.first)
-      return atom -> op.second(getfield(atom,symbol),val)
+function (key::Keyword)(s::AbstractVector{<:AbstractString})
+  @unpack name, field, operators = key 
+  for op in operators
+    if (i = has_key(op.first,s)) > 0
+      return el -> op.second(getfield(el,field),parse_to_type(key,s[i+1]))
     end
   end
-  # If got here, no operator was found, then we assume is is an implicit equal
-  val = parse_keyword(key,str," ")
-  return atom -> isequal(getfield(atom,symbol),val)
+  # If no operator was found, assume that `=` was intended
+  i = has_key(name,s)
+  return el -> isequal(getfield(el,field),parse_to_type(key,s[i+1]))
 end
 
-numerical_keywords = [ NumericalKeyword("index", :index, operators), 
-                       NumericalKeyword("index_pdb",  :index_pdb, operators),
-                       NumericalKeyword("resnum", :resnum, operators),
-                       NumericalKeyword("residue", :residue, operators),
-                       NumericalKeyword("b", :b, operators),
-                       NumericalKeyword("occup", :occup, operators),
-                       NumericalKeyword("model", :model, operators),
-                     ]
+"""
 
-string_keywords = [ StringKeyword("name", :name, operators), 
-                    StringKeyword("segname", :segname, operators), 
-                    StringKeyword("resname", :resname, operators), 
-                    StringKeyword("chain", :chain, operators), 
-                    StringKeyword("element", :element, operators), 
-                  ]
+`FunctionalKeyword{T}`
 
-#
-# Special functions keywords
-#
+This is a structure that will store a keyword that depends on an external function
+requiring an operator and an argument. 
 
-struct MacroKeyword
+## Example:
+
+```
+element_keyword = FunctionalKeyword(String,      
+                                    "element",
+                                    element,
+                                    ("=",isequal))
+
+```
+will define a keyword "element" to be used as `element C`, which will return
+`true` if there is an `element` function such that `element(atom) == C`.
+
+"""
+struct FunctionalKeyword{FunctionType}
+  ValueType :: Type
   name :: String
-  fname :: Function
+  by :: FunctionType
+  operators :: Tuple 
 end
-getfunc(key::MacroKeyword,str) = key.fname
+
+function (key::FunctionalKeyword)(s::AbstractVector{<:AbstractString})
+  @unpack name, by, operators = key 
+  for op in operators
+    if (i = has_key(op.first,s)) > 0
+      return el -> op.second(by(el),parse_to_type(key,s[i+1]))
+    end
+  end
+  # If no operator was found, assume that `=` was intended
+  i = has_key(name,s)
+  return el -> isequal(by(el),parse_to_type(key,s[i+1]))
+end
+
+#
+# Macro keywords (functions without parameters)
+#
+
+struct MacroKeyword{FunctionType}
+  name :: String
+  by :: FunctionType
+end
+
+function (key::MacroKeyword)(s::AbstractVector{<:AbstractString})
+  return key.by
+end
+
+"""
+
+`parse_to_type(key::Keyword, val::String)`
+
+Tries to parse `val` into the type of value expected by `key.ValueType`. 
+
+"""
+function parse_to_type(key::Union{Keyword,FunctionalKeyword},val)
+  if key.ValueType == String
+    return val
+  end
+  try 
+    val = parse(key.ValueType,val)
+    return val
+  catch
+    parse_error("Could not parse $val for keyword $(key.name), expected $(key.ValueType)")
+  end
+end
+
+#
+# Keywords for PDBTools
+#
+
+keywords = [ Keyword(Int,"index", :index, operators), 
+             Keyword(Int,"index_pdb",  :index_pdb, operators),
+             Keyword(Int,"resnum", :resnum, operators),
+             Keyword(Int,"residue", :residue, operators),
+             Keyword(Float64,"b", :b, operators),
+             Keyword(Float64,"occup", :occup, operators),
+             Keyword(Int,"model", :model, operators),
+             Keyword(String,"name", :name, operators), 
+             Keyword(String,"segname", :segname, operators), 
+             Keyword(String,"resname", :resname, operators), 
+             Keyword(String,"chain", :chain, operators), 
+            ]
 
 macro_keywords = [ MacroKeyword("water", iswater), 
                    MacroKeyword("protein", isprotein), 
@@ -138,54 +177,85 @@ macro_keywords = [ MacroKeyword("water", iswater),
                    MacroKeyword("all", isequal), 
                  ]
 
-#
-# Remove trailing and multiple spaces
-#
+functional_keywords = [ FunctionalKeyword(String, "element", element, operators) ]
 
-function remove_spaces(str)
-  str = split(str)
-  s = str[1]
-  for i in 2:length(str)
-    s = s*' '*str[i]
-  end
-  return " "*s*" "
-end
-
+#
 # parse_query and apply_query are a very gentle contribution given by 
 # CameronBieganek in https://discourse.julialang.org/t/parsing-selection-syntax/43632/9
 # while explaining to me how to creat a syntex interpreter
+#
+"""
 
-function parse_query(selection)
-  # Remove spaces
-  s = remove_spaces(selection) 
+`has_key(key::String, s::AbstractVector{<:AbstractString})`
 
-  # Parse syntax
-  try
-    if occursin(" or ", s)
-      (|, parse_query.(split(s, "or"))...)
-    elseif occursin(" and ", s)
-      (&, parse_query.(split(s, "and"))...)
-    elseif occursin(" not ", s)
-      rest = match(r".*not(.*)", s)[1]
-      (!, parse_query(rest))
+Returns the first index of the vector `s` in which where `key` is found, or 0. 
 
-    # keywords 
-    else
-      for key in numerical_keywords
-        occursin(" "*key.name*" ", s) && return getfunc(key,s)
+## Example:
+
+```julia-repl
+
+julia> PDBTools.has_key("or",["name","CA","or","index","1"])
+3
+
+julia> PDBTools.has_key("and",["name","CA","or","index","1"])
+0
+
+```
+
+"""
+function has_key(key::String ,s::AbstractVector{<:AbstractString})
+  i = findfirst(isequal(key),s)
+  if i == nothing
+    0
+  else
+    i
+  end
+end
+
+"""
+
+`parse_query(selection:String)`
+
+"""
+parse_query(selection::String) = parse_query_vector(split(selection))
+
+"""
+
+`parse_query_vector(s::AbstractVector{<:AbstractString})`
+
+"""
+function parse_query_vector(s)
+  if (i = has_key("or",s)) > 0
+    deleteat!(s,i)
+    (|, parse_query_vector.((s[1:i-1],s[i:end]))...)
+  elseif (i = has_key("and",s)) > 0
+    deleteat!(s,i)
+    (&, parse_query_vector.((s[1:i-1],s[i:end]))...)
+  elseif (i = has_key("not",s)) > 0
+    deleteat!(s,i)
+    (!, parse_query_vector(s[i:end]))
+
+  # keywords 
+  else
+    for key in keywords
+      if (i = has_key(key.name,s)) > 0 
+        deleteat!(s,i)
+        return key(s)
       end
-      for key in string_keywords
-        occursin(" "*key.name*" ", s) && return getfunc(key,s)
-      end
-      for key in macro_keywords
-        occursin(" "*key.name*" ", s) && return getfunc(key,s)
-      end
-      parse_error()
     end
-
-  # Error in the syntax
-  catch err
-    parse_error()
+    for key in macro_keywords
+      if (i = has_key(key.name,s)) > 0 
+        deleteat!(s,i)
+        return key(s)
+      end
+    end
+    for key in functional_keywords
+      if (i = has_key(key.name,s)) > 0 
+        deleteat!(s,i)
+        return key(s)
+      end
+    end
+    parse_error("Unable to parse selection string.")
   end
 end
 
@@ -203,14 +273,16 @@ end
 #
 
 struct NoBackTraceException
-    exc::Exception
+  exc::Exception
 end
 
 function Base.showerror(io::IO, ex::NoBackTraceException, bt; backtrace=true)
-    Base.with_output_color(get(io, :color, false) ? Base.error_color() : :nothing, io) do io
-        showerror(io, ex.exc)
-    end
+  Base.with_output_color(get(io, :color, false) ? Base.error_color() : :nothing, io) do io
+    showerror(io, ex.exc)
+  end
 end
 
-parse_error() = throw(NoBackTraceException(ErrorException("Error parsing selection. Use spaces, parenthesis not supported.")))
+parse_error(str) = throw(NoBackTraceException(ErrorException(str)))
+
+
 
