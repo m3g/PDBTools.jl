9210058081ab92246519e440dd0f24120c20c7af
lmiq <lmartine@unicamp.br>
2024-11-13 11:45:43 -0300

    read pdb atom in type-stable way

--- src/readPDB.jl
+++ src/readPDB.jl
@@ -67,34 +67,28 @@ function _parse_pdb(
     pdbdata::Union{IOStream, IOBuffer}, 
     only::Function, 
 )
-    natoms = 0
-    index = 0
     imodel = 1
-    iresidue = 0
     atoms = Atom[]
-    lastatom = Atom()
+    lastatom = Atom(; index=Int32(0), residue=Int32(0))
     for line in eachline(pdbdata)
         if occursin("END", line)
             imodel = imodel + 1
         end
         atom = read_atom_PDB(line)
         if !isnothing(atom)
-            index += 1
-            atom.index = index
+            atom.index = index(lastatom) + 1
             atom.model = imodel
             if !same_residue(atom, lastatom)
-                iresidue += 1
+                atom.residue = residue(lastatom) + 1
             end
-            atom.residue = iresidue
             if only(atom)
-                natoms += 1
                 push!(atoms, atom)
             end
             lastatom = atom
         end
     end
     seekstart(pdbdata)
-    if natoms == 0
+    if length(atoms) == 0
         throw(ArgmentError("""\n 
             Could not find any atom in PDB file matching the selection. 
 
@@ -102,6 +96,44 @@ function _parse_pdb(
     end
     return atoms
 end
+
+# read atom from PDB file
+function read_atom_PDB(record::String, atom = Atom(;index=Int32(1), model=Int32(1), custom=nothing))
+    !startswith(record, r"ATOM|HETATM") && return nothing
+    inds_and_names = (
+        (1, Val(:name)), 
+        (2, Val(:resname)), 
+        (3, Val(:chain)), 
+        (4, Val(:index_pdb)), 
+        (5, Val(:resnum)), 
+        (6, Val(:x)), 
+        (7, Val(:y)), 
+        (8, Val(:z)), 
+        (9, Val(:occup)), 
+        (10,Val(:beta)), 
+        (11,Val(:segname)), 
+        (12,Val(:pdb_element)), 
+        (13,Val(:charge)),
+    )
+    @views field_values = (
+        record[13:16], # :name
+        record[17:21], # :resname
+        record[22:22], # :chain
+        record[7:11],  # :index_pdb
+        record[23:26], # :resnum
+        record[31:38], # :x
+        record[39:46], # :y
+        record[47:54], # :z
+        length(record) >= 60 ? record[56:60] : record[56:end], # :occup
+        length(record) >= 66 ? record[61:66] : record[61:end], # :beta
+        length(record) >= 76 ? record[73:76] : record[73:end], # :segname
+        length(record) >= 78 ? record[77:78] : record[77:end], # :pdb_element
+        length(record) >= 80 ? record[79:80] : record[79:end], # :charge 
+    )
+    setfield_recursive!(atom, field_values, inds_and_names)
+    return atom
+end
+
 
 @testitem "readPDB" begin
     pdb_file = "$(@__DIR__)/../test/structure.pdb"
@@ -111,4 +143,61 @@ end
     atoms = readPDB(IOBuffer(pdbdata), "protein and name CA")
     @test length(atoms) == 104
 end
+
+@testitem "read_atom_pdb" begin
+    line = "HETATM    1  O1  BGL    1       0.665   1.214  -0.259  1.00  0.00"
+    a = PDBTools.read_atom(line)
+    @test a.index == 1
+    @test a.name == "O1"
+    @test a.resname == "BGL"
+    @test a.chain == "X"
+    @test a.resnum == 1
+    @test a.residue == 0
+    @test (a.x, a.y, a.z) == (0.665, 1.214, -0.259)
+    @test (a.occup, a.beta) == (1.0, 0.0)
+    @test a.model == 1
+    @test a.segname == "-"
+    @test a.index_pdb == 1
+    @test a.pdb_element == "X"
+    @test isnothing(a.charge)
+
+    line = "HETATM    1  O1  AGL     1       0.803   1.186  -0.211  1.00  0.00            1+  "
+    a = PDBTools.read_atom(line)
+    @test a.index == 1
+    @test a.name == "O1"
+    @test a.resname == "AGL"
+    @test a.chain == "X"
+    @test a.resnum == 1
+    @test a.residue == 0
+    @test (a.x, a.y, a.z) == (0.803, 1.186, -0.211)
+    @test (a.occup, a.beta) == (1.0, 0.0)
+    @test a.model == 1
+    @test a.segname == "-"
+    @test a.index_pdb == 1
+    @test a.pdb_element == "X"
+    @test a.charge == "1+"
+
+    line = "ATOM  *****  H2  GLYCD4301      11.014  36.823  40.115  1.00  0.00      GLYC H"
+    a = PDBTools.read_atom(line)
+    @test a.index == 1
+    @test a.name == "H2"
+    @test a.resname == "GLYC"
+    @test a.chain == "D"
+    @test a.resnum == 4301
+    @test a.residue == 0
+    @test (a.x, a.y, a.z) == (11.014, 36.823, 40.115)
+    @test (a.occup, a.beta) == (1.0, 0.0)
+    @test a.model == 1
+    @test a.segname == "GLYC"
+    @test a.index_pdb == -1
+    @test a.pdb_element == "H"
+    @test isnothing(a.charge)
+
+    line = "ATOM  3ce2c  LP2 WLS Cffff     376.512 638.670  16.990  0.00  0.00         C"
+    a = PDBTools.read_atom(line)
+    @test a.index == 1
+    @test a.index_pdb == 249388
+    @test a.resnum == 65535
+end
+
 
