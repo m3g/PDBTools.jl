6583775f1e5afdb4c5e3bf50734c48d0cb62277f
lmiq <lmartine@unicamp.br>
2024-11-14 12:36:39 -0300

    fix function call

--- src/legacy.jl
+++ src/legacy.jl
@@ -4,4 +4,4 @@
 export readPDB, writePDB
 const readPDB = read_pdb
 writePDB(atoms::AbstractVector{<:Atom}, filename::String, args...; kargs...) = 
-    write_pdb(filename, atoms; args...; kargs...)
+    write_pdb(filename, atoms, args...; kargs...)
