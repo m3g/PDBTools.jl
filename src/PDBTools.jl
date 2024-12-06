49c957fccee42445d2ae4337ad4f92d180d2a95f
Ana Beatriz Lima <ana.blima@protonmail.com>
2024-12-06 13:36:59 -0300

    changed the path to chains.pdb

--- src/PDBTools.jl
+++ src/PDBTools.jl
@@ -47,7 +47,7 @@ const TESTPDB = joinpath(@__DIR__,"../test/structure.pdb")
 const SMALLPDB = joinpath(@__DIR__,"../test/small.pdb")
 const SIRAHPDB = joinpath(@__DIR__,"../test/sirah.pdb")
 const TESTCIF = joinpath(@__DIR__,"../test/small.cif")
-const CHAINSPDB = joinpath(@__DIR__,"../test/protein_test.pdb")
+const CHAINSPDB = joinpath(@__DIR__,"../test/chains.pdb")
 
 # Basic chemistry
 include("./elements.jl")
