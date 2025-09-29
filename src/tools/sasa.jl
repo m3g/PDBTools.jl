a282b0820e8acf21dffb119f5776b6524f4c992b
lmiq <lmartine@unicamp.br>
2025-09-29 09:58:56 -0300

    add documentation, update interface

--- src/tools/sasa.jl
+++ src/tools/sasa.jl
@@ -3,20 +3,28 @@ using CellListMap: ParticleSystem, map_pairwise!
 using LinearAlgebra
 using Statistics: mean
 
+# Container for the custom atom filed that will carry the atom SASA
 struct SASA sasa::Float32 end
 
-"""
-    generate_dots(radius, n_grid_points)
+#=
+    generate_dots(atomi_radius, probe_radius, n_dots)
 
-Generates points on the surface of a sphere of a given `radius` using the 
-double cubic lattice method. `n_grid_points` controls the density of the points.
-"""
-function generate_dots(radius::Real, n_grid_points::Int)
-    dots = SVector{3,Float32}[]
+Generates points on the surface of a sphere of a given radius (atomic_radius + probe_radius) 
+using the double cubic lattice method. `n_dots` controls the maximum number of dots
+per sphere.
+
+=#
+function generate_dots(atomic_radius, probe_radius::Real, n_dots::Int)
+    radius = atomic_radius + probe_radius
+    n_grid_points = round(Int, (3/(4 * π * ((1/2)^3)) * n_dots)^(1/3))
     if radius <= 0 || n_grid_points <= 0
-        return dots
+        throw(ArgumentError("""\n
+            The probe_radius ($probe_radius) or number of dots ($n_dots) are too small, or incorrectly provided.
+
+        """))
     end
-    
+
+    dots = SVector{3,Float32}[]
     step = 2 * radius / n_grid_points
     radius_sq = radius^2
 
@@ -53,31 +61,36 @@ function generate_dots(radius::Real, n_grid_points::Int)
     return unique!(dots)
 end
 
-struct AtomDots0
+#
+# Structure that carries the information about dots of each atom, if 
+# they are exposed or found to be occluded by other atoms.
+#
+struct AtomDots
     exposed::Vector{Bool}
 end
-function CellListMap.reset_output!(x::AtomDots0) 
+function CellListMap.reset_output!(x::AtomDots) 
     x.exposed .= true
     return x
 end
-CellListMap.copy_output(x::AtomDots0) = AtomDots0(copy(x.exposed))
-function CellListMap.reducer(x::AtomDots0, y::AtomDots0) 
+CellListMap.copy_output(x::AtomDots) = AtomDots(copy(x.exposed))
+function CellListMap.reducer(x::AtomDots, y::AtomDots) 
     x.exposed .= x.exposed .& y.exposed
     return x
 end
 
-function update_dot_oclusion!(
+function update_dot_exposure!(
     i, j, x, y, atoms, dot_cache, surface_dots; 
     atom_type::Function,
     atom_radius_from_type::Function,
     probe_radius,
 )
     type_i = atom_type(atoms[i])
-    R_j_sq = (atom_radius_from_type(atom_type(atoms[j])) + probe_radius)^2
+    type_j = atom_type(atoms[j])
+    R_j_sq = (atom_radius_from_type(type_j) + probe_radius)^2
     for (idot, dot_exposed) in enumerate(surface_dots[i].exposed)
         if dot_exposed
-            # Position the dot on the atom's surface in the molecule's coordinate system
             dot_on_surface = x + dot_cache[type_i][idot]
+            # Position the dot on the atom's surface in the molecule's coordinate system
             # Check if the dot is inside the neighboring atom j
             if sum(abs2, dot_on_surface - y) < R_j_sq
                 surface_dots[i].exposed[idot] = false
@@ -87,32 +100,78 @@ function update_dot_oclusion!(
     return nothing
 end
 
-function pair_dot_oclusion!(
+function update_pair_dot_exposure!(
     x, y, i, j, surface_dots; 
     atoms, dot_cache, atom_type::F1, atom_radius_from_type::F2, probe_radius,
 ) where {F1,F2}
-    update_dot_oclusion!(i, j, x, y, atoms, dot_cache, surface_dots; atom_type, atom_radius_from_type, probe_radius)
-    update_dot_oclusion!(j, i, y, x, atoms, dot_cache, surface_dots; atom_type, atom_radius_from_type, probe_radius)
+    update_dot_exposure!(i, j, x, y, atoms, dot_cache, surface_dots; atom_type, atom_radius_from_type, probe_radius)
+    update_dot_exposure!(j, i, y, x, atoms, dot_cache, surface_dots; atom_type, atom_radius_from_type, probe_radius)
     return surface_dots
 end
 
 """
-    sasa(atoms; probe_radius, n_grid_points)
+    atomic_sasa(atoms; probe_radius, n_dots)::Vector{PDBTools.Atom{SASA}}
 
-Calculates the Solvent Accessible Surface Area (SASA) for a vector of `Atom`s.
+Calculates the Solvent Accessible Surface Area (SASA) for a vector of `Atom`s. 
+Returns a vector of `PDBToools.Atom`s, with a custom fiels with `PDBTools.SASA` 
+structure, which contains the solvent accessible surface area for that atom.
 
-# Arguments
+The `sasa` function computes the total SASA or the SASA of a subset of the atoms
+in the structure. The output value is provided in Å^2. 
+
+# Main argument
 
 - `atoms::Vector{PDBTools.Atom}`: A vector of atoms in the molecule.
+
+# Optional arguments 
+
 - `probe_radius::Real=1.4`: The radius of the solvent probe in Angstroms.
-- `n_grid_points::Int=20`: The number of grid points along one axis for dot generation. 
+- `n_dots::Int=500`: The number of grid points along one axis for dot generation. 
   Higher values lead to more accurate but slower calculations.
+- `parallel::Bool=true`: Control if the computation runs in parallel (requires 
+  running Julia with multiple threads).
+
+# Example
+
+```jldoctest
+julia> using PDBTools
+
+julia> prot = select(read_pdb(PDBTools.TESTPDB), "protein");
+
+julia> at_sasa = atomic_sasa(prot);
+
+julia> sasa(at_sasa) # total sasa of prot
+5374.4414f0
+
+julia> sasa(at_sasa, "backbone") # backbone sasa in prot
+873.72626f0
+
+julia> sasa(at_sasa, "not backbone") # other atoms
+4500.7124f0
+
+julia> sasa(at_sasa, "resname ARG GLU") # some residue types
+531.51776f0
+```
+
+# Additional control:
+
+Two arguments can control the atom radii used for computing the SASA. These arguments
+are functions:
+
+- `atom_type`: Function that given each atom of the array of atoms, returns the atom "type".
+- `atom_radius_from_type`: Given the atom "type", returns the vdW radius of the atom. 
+
+By default, `atom_type = PDBTools.element`, a function that just returns the element of the atom,
+and `atom_radius_from_type` obtains the vdW radius from the `PDBTools.elements` list, in which
+the atomc radii of https://en.wikipedia.org/wiki/Atomic_radii_of_the_elements_(data_page) are See the documentation for further information.
+used. Atoms with missing radius have a `NaN` value, and the computation will not return meaningful
+values. See the documentation for further information.
 
 """
-function compute_sasa(
+function atomic_sasa(
     atoms::AbstractVector{<:Atom}; 
-    probe_radius::Real = 1.4, 
-    n_grid_points::Int = 20,
+    probe_radius::Real=1.4, 
+    n_dots::Int=500,
     atom_type::Function = element,
     atom_radius_from_type::Function = type -> getproperty(elements[type], :vdw_radius),
     parallel=true,
@@ -124,21 +183,21 @@ function compute_sasa(
     # Memoization for dot generation to avoid recomputing for same radii
     dot_cache = Dict{eltype(atom_types), Vector{SVector{3,Float32}}}()
     for type in atom_types
-        dot_cache[type] = generate_dots(atom_radius_from_type(type) + probe_radius, n_grid_points)
+        dot_cache[type] = generate_dots(atom_radius_from_type(type), probe_radius, n_dots)
     end
         
     system = ParticleSystem(
         xpositions = coor.(atoms),
         unitcell = nothing,
-        cutoff = 2*maximum(atom_radius_from_type(type) for type in atom_types) + probe_radius,
-        output = [ AtomDots0(ones(Bool, length(dot_cache[atom_type(at)]))) for at in atoms ],
+        cutoff = 2*(maximum(atom_radius_from_type(type) for type in atom_types) + probe_radius),
+        output = [ AtomDots(ones(Bool, length(dot_cache[atom_type(at)]))) for at in atoms ],
         output_name = :surface_dots,
         parallel=parallel,
     )
 
     map_pairwise!(
         (x, y, i, j, d2, surface_dots) -> 
-            pair_dot_oclusion!(
+            update_pair_dot_exposure!(
                 x, y, i, j, surface_dots; 
                 atoms, dot_cache, atom_type, atom_radius_from_type, probe_radius,
             ),
@@ -158,7 +217,40 @@ function compute_sasa(
     return ats_with_sasa
 end
 
-export compute_sasa, sasa
+"""
+    sasa(atom::Atom{PDBTools.SASA})
+    sasa(atoms::AbstractVector{PDBTools.SASA})
+    sasa(atoms::AbstractVector{PDBTools.SASA}, selection::Union{Function,String})
+
+Given the output of `atomic_sasa`, sums up contributions of atoms to compute the SASA
+of the full structure, an atom, or a subset of atoms. The function can called with a 
+single `Atom{SASA}` atom, a vector of atoms (in which case the full SASA is returned),
+or a vector of atoms and a selection, given by a function or selection string. 
+
+# Example
+
+```jldoctest
+julia> using PDBTools
+
+julia> prot = select(read_pdb(PDBTools.TESTPDB), "protein");
+
+julia> at_sasa = atomic_sasa(prot);
+
+julia> sasa(at_sasa) # total sasa of prot
+5374.4414f0
+
+julia> sasa(at_sasa, "backbone") # selection string
+873.72626f0
+
+julia> sasa(at_sasa, at -> name(at) == "CA") # selection function
+20.5797f0
+
+julia> sasa(at_sasa[1]) # single atom SASA
+1.8389939f0
+```
+
+"""
+function sasa end
 
 sasa(atom::Atom{SASA}) = atom.custom.sasa
 sasa(atoms::AbstractVector{<:Atom{SASA}}) = sum(sasa, atoms)
@@ -169,19 +261,7 @@ end
 
 @testitem "sasa" begin
 
-    # gmx sasa -s prot.pdb -probe 1.4 -ndots 20 -n index.ndx -surface CYSTHR -output CYSTHR
-    # Using the apparent vdW radii that Gromacs uses
-    gmx_radii = Dict(
-        "N" => 1.555,
-        "C" => 1.570,
-        "H" => 1.520,
-        "O" => 1.552,
-        "S" => 1.575,
-    )
-    atom_radius_from_type(type) = gmx_radii[type]
-
-    # gmx sasa -s prot.pdb -probe 1.4 -ndots 20 -n index.ndx -surface CYSTHR -output CYSTHR
-    # Using the apparent vdW radii that Gromacs uses
+    # Using VMD radii
     vmd_radii = Dict(
         "N" => 1.55,
         "C" => 1.70,
@@ -189,32 +269,18 @@ end
         "O" => 1.52,
         "S" => 1.80,
     )
-    atom_radius_from_type(type) = vmd_radii[type]
-
-    sasa_gmx = Dict(
-        "total" => 115.384,
-        "first_atom" => 0.0,
-        "1st_and_last (isolated)" => 43.657,
-        "first_residue (isolated)" => 34.182,
-        "CYS and THR" => 7.734,
-        "CYS and THR (isolated)" => 80.959,
-        "N atom (isolated)" => 30.386,
-        "S atom (isolated)" => 31.171,
-        "C atom (isolated)" => 30.975,
-        "O atom (isolated)" => 30.269,
-        "H atom (isolated)" => 29.033,
-    )
 
     prot = read_pdb(PDBTools.TESTPDB, "protein")
-    @test sasa(compute_sasa([prot[1]])) ≈ sasa_gmx["first atom (isolated)"] rtol = 1e-3
-    @test sasa(compute_sasa([prot[20]])) ≈ sasa_gmx["S atom (isolated)"] rtol = 1e-3
-    @test sasa(compute_sasa([prot[5]])) ≈ sasa_gmx["C atom (isolated)"] rtol = 1e-3
-    @test sasa(compute_sasa([prot[12]])) ≈ sasa_gmx["C atom (isolated)"] rtol = 1e-3
-    @test sasa(compute_sasa([prot[2]])) ≈ sasa_gmx["H atom (isolated)"] rtol = 1e-3
+
+    @test sasa(atomic_sasa([prot[1]])) ≈ sasa_gmx["first atom (isolated)"] rtol = 1e-3
+    @test sasa(atomic_sasa([prot[20]])) ≈ sasa_gmx["S atom (isolated)"] rtol = 1e-3
+    @test sasa(atomic_sasa([prot[5]])) ≈ sasa_gmx["C atom (isolated)"] rtol = 1e-3
+    @test sasa(atomic_sasa([prot[12]])) ≈ sasa_gmx["C atom (isolated)"] rtol = 1e-3
+    @test sasa(atomic_sasa([prot[2]])) ≈ sasa_gmx["H atom (isolated)"] rtol = 1e-3
 
 
     # For comparison, the SASA of a single, isolated atom
-    isolated_atom_sasa = calculate_sasa([atom1], probe_radius=1.4, n_grid_points=25)
+    isolated_atom_sasa = atomic_sasa([atom1], probe_radius=1.4)
     
     
     println("SASA of the two-atom molecule: ", round(sasa_value, digits=2), " Å²")
