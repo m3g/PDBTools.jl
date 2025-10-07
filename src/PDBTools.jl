835d431c252ad82493c5e37b76748fc140ff59d4
lmiq <lmartine@unicamp.br>
2025-10-07 13:50:55 -0300

    add support for PBCs in SASA calculation

--- src/PDBTools.jl
+++ src/PDBTools.jl
@@ -56,6 +56,7 @@ const LONG_CHAIN_STRING_CIF = joinpath(@__DIR__, "../test/long_chain_string.cif"
 const LONG_NAME_STRING_CIF = joinpath(@__DIR__, "../test/long_name_string.cif")
 const DIMERPDB = joinpath(@__DIR__, "../test/dimer.pdb")
 const HETATMPDB = joinpath(@__DIR__, "../test/hetatm.pdb")
+const TESTPBC = joinpath(@__DIR__, "../test/pbc.pdb")
 
 # Basic chemistry
 include("./properties/elements.jl")
