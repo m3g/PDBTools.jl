878d78441388588408e546321bd7caa6ea4e17da
lmiq <lmartine@unicamp.br>
2025-10-17 11:36:07 -0300

    hbonds for single set seem to be working

---
+++ src/tools/hydrogen_bonds.jl
@@ -0,0 +1,130 @@
+
+struct HPolarBonds
+    D::Vector{Int32} # Hydrogen-bond donnor
+    H::Vector{Int32} # polar hydrogen
+end
+CellListMap.copy_output(x::HPolarBonds) = HPolarBonds(copy(x.D), copy(x.H))
+function CellListMap.reset_output!(x::HPolarBonds) 
+    empty!(x.D)
+    empty!(x.H)
+    return x
+end
+CellListMap.reducer!(x::HPolarBonds, y::HPolarBonds) = append!(x,y)
+
+struct HBonds
+    D::Vector{Int32} # Hydrogen bond donnor
+    H::Vector{Int32} # polar hydrogen
+    A::Vector{Int32} # hydrogen bond acceptor
+end
+Base.length(x::HBonds) = length(x.D)
+Base.getindex(x::HBonds, i::Integer) = (x.D[i], x.H[i], x.A[i])
+
+CellListMap.copy_output(x::HBonds) = HBonds(copy(x.D), copy(x.H), copy(x.A))
+function CellListMap.reset_output!(x::HBonds)
+    empty!(x.D)
+    empty!(x.H)
+    empty!(x.A)
+    return x
+end 
+CellListMap.reducer!(x::HBonds, y::HBonds) = append!(x,y)
+
+function hbond_angle(n1,h,n2)
+    v1 = h - n1 
+    v2 = n2 - h
+    return acosd(dot(v1,v2) / (norm(v1)*norm(v2)))
+end
+
+function push_hbond!(i, j, x, y, polar_bonds, sys, ang, hbonds, ats_sel1)
+    # Find if i is a donnor
+    ii = searchsortedfirst(polar_bonds.D, i)
+    ii > length(polar_bonds.D) && return nothing
+    # Might have more than one polar hydrogen
+    while polar_bonds.D[ii] == i
+        iH = polar_bonds.H[ii]
+        xH = wrap(sys.positions[iH], x, sys.unitcell)
+        if hbond_angle(x, xH, y) <= ang
+            push!(hbonds.D, index(ats_sel1[i]))
+            push!(hbonds.H, index(ats_sel1[iH]))
+            push!(hbonds.A, index(ats_sel1[j]))
+        end
+        ii += 1
+        ii > length(polar_bonds.D) && break
+    end
+    return nothing
+end
+
+function hydrogen_bonds(
+    ats::AbstractVector{<:PDBTools.Atom},
+    sel1::Union{Function, String}=at -> true;
+    donnor_acceptor_distance::Real=3.0f0,
+    angle_cutoff::Real=20,
+    electronegative_elements=("N", "O", "F", "S"),
+    unitcell::Union{Nothing,AbstractVecOrMat}=nothing,
+    d_covalent_bond::Real=1.2f0,
+    parallel::Bool=false,
+)
+    # Select desired atoms
+    ats_sel1 = select(ats, sel1)
+    ats_sel1_positions = coor.(ats_sel1)
+    #
+    # Find all possible hydrogen-bonding donors
+    #
+    sys_polar_bonds = ParticleSystem(
+        positions=ats_sel1_positions,
+        unitcell=unitcell,
+        cutoff=donnor_acceptor_distance,
+        output=HPolarBonds(Int32[], Int32[]),
+        parallel=parallel,
+    )
+    polar_bonds = map_pairwise!(
+        (x,y,i,j,d2,polar_bonds) -> begin
+            at_i = ats_sel1[i]
+            at_j = ats_sel1[j]
+            el_i = element(at_i)
+            el_j = element(at_j)
+            if (el_i in electronegative_elements) & (el_j == "H")
+                D = i
+                H = j
+            elseif (el_j in electronegative_elements) & (el_i == "H")
+                D = j
+                H = i
+            else
+                return polar_bonds 
+            end
+            if d2 < (d_covalent_bond)^2
+                push!(polar_bonds.D, D)
+                push!(polar_bonds.H, H)
+            end
+            return polar_bonds
+        end,
+        sys_polar_bonds,
+    )
+    iord = sortperm(polar_bonds.D)
+    polar_bonds.D .= polar_bonds.D[iord]
+    polar_bonds.H .= polar_bonds.H[iord]
+
+    #
+    # Now, find all hydrogen bonds
+    #
+    sys_hbonds = ParticleSystem(
+        positions=ats_sel1_positions,
+        unitcell=unitcell,
+        cutoff=donnor_acceptor_distance,
+        output=HBonds(Int32[], Int32[], Int32[]),
+        parallel=parallel,
+        output_name=:hbonds
+    )
+    map_pairwise!(
+        (x,y,i,j,d2,hbonds) -> begin
+            el_i = element(ats_sel1[i])
+            el_j = element(ats_sel1[j])
+            if (el_i in electronegative_elements) & (el_j in electronegative_elements) 
+                push_hbond!(i, j, x, y, polar_bonds, sys_hbonds, angle_cutoff, hbonds, ats_sel1)
+                push_hbond!(j, i, y, x, polar_bonds, sys_hbonds, angle_cutoff, hbonds, ats_sel1)
+            end
+            return hbonds
+        end,
+        sys_hbonds,
+    )
+    return sys_hbonds.hbonds
+end
