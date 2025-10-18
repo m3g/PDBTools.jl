47d74ab228dc6e665ac7ca793bd3b62022f53b4f
lmiq <lmartine@unicamp.br>
2025-10-18 14:05:30 -0300

    format docs

--- src/tools/hydrogen_bonds.jl
+++ src/tools/hydrogen_bonds.jl
@@ -4,14 +4,14 @@ struct HPolarBonds
     H::Vector{Int32} # polar hydrogen
 end
 CellListMap.copy_output(x::HPolarBonds) = HPolarBonds(copy(x.D), copy(x.H))
-function CellListMap.reset_output!(x::HPolarBonds) 
+function CellListMap.reset_output!(x::HPolarBonds)
     empty!(x.D)
     empty!(x.H)
     return x
 end
 function CellListMap.reducer!(x::HPolarBonds, y::HPolarBonds)
-    append!(x.D,y.D)
-    append!(x.H,y.H)
+    append!(x.D, y.D)
+    append!(x.H, y.H)
     return x
 end
 
@@ -42,20 +42,20 @@ function CellListMap.reset_output!(x::HBonds)
     empty!(x.r)
     empty!(x.ang)
     return x
-end 
-function CellListMap.reducer!(x::HBonds, y::HBonds) 
-    append!(x.D,y.D)
-    append!(x.H,y.H)
-    append!(x.A,y.A)
-    append!(x.r,y.r)
-    append!(x.ang,y.ang)
+end
+function CellListMap.reducer!(x::HBonds, y::HBonds)
+    append!(x.D, y.D)
+    append!(x.H, y.H)
+    append!(x.A, y.A)
+    append!(x.r, y.r)
+    append!(x.ang, y.ang)
     return x
 end
 
-function hbond_angle(D,H,A)
+function hbond_angle(D, H, A)
     v1 = H - D
-    v2 = A - D 
-    return acosd(dot(v1,v2) / (norm(v1)*norm(v2)))
+    v2 = A - D
+    return acosd(dot(v1, v2) / (norm(v1) * norm(v2)))
 end
 
 function push_hbond!(hbonds, i, j, x, y, polar_bonds, positions, unitcell, ang, ats_sel1, d2)
@@ -82,8 +82,8 @@ end
 
 function find_hbond_donnors(
     ats_sel;
-    positions, 
-    unitcell, 
+    positions,
+    unitcell,
     cutoff,
     parallel,
     electronegative_elements,
@@ -98,7 +98,7 @@ function find_hbond_donnors(
         output_name=:polar_bonds,
     )
     polar_bonds = map_pairwise!(
-        (x,y,i,j,d2,polar_bonds) -> begin
+        (x, y, i, j, d2, polar_bonds) -> begin
             at_i = ats_sel[i]
             at_j = ats_sel[j]
             el_i = element(at_i)
@@ -110,7 +110,7 @@ function find_hbond_donnors(
                 D = j
                 H = i
             else
-                return polar_bonds 
+                return polar_bonds
             end
             if d2 < (d_covalent_bond)^2
                 push!(polar_bonds.D, D)
@@ -148,7 +148,7 @@ Function to find hydrogen bonds in a set of atoms.
 
 - `ats::AbstractVector{<:PDBTools.Atom}`: Vector of atoms to analyze.
 and
-- `sel::Union{Function, String}=at -> true`: Selection of atoms to consider. Can be a function or a selection string.
+- `sel::Union{Function, String}=at -> true`: Selection of atoms to consider. Can be a function or a selection string. This selection is optional.
 or
 - `sel1::Union{Function, String}`: First selection of atoms to consider. Can be a function or a selection string.
 - `sel2::Union{Function, String}`: Second selection of atoms to consider. Can be a function or a selection string.
@@ -207,7 +207,7 @@ julia> hbs[1]
 """
 function hydrogen_bonds(
     ats::AbstractVector{<:PDBTools.Atom},
-    sel::Union{Function, String}=at -> true;
+    sel::Union{Function,String}=at -> true;
     donnor_acceptor_distance::Real=3.5f0,
     angle_cutoff::Real=30,
     electronegative_elements=("N", "O", "F", "S"),
@@ -224,8 +224,8 @@ function hydrogen_bonds(
     #
     polar_bonds = find_hbond_donnors(
         ats_sel;
-        positions=ats_sel_positions, 
-        unitcell, 
+        positions=ats_sel_positions,
+        unitcell,
         cutoff=donnor_acceptor_distance,
         parallel,
         electronegative_elements,
@@ -244,18 +244,18 @@ function hydrogen_bonds(
         output_name=:hbonds
     )
     map_pairwise!(
-        (x,y,i,j,d2,hbonds) -> begin
+        (x, y, i, j, d2, hbonds) -> begin
             el_i = element(ats_sel[i])
             el_j = element(ats_sel[j])
-            if (el_i in electronegative_elements) & (el_j in electronegative_elements) 
-                push_hbond!(hbonds, 
-                    i, j, x, y, polar_bonds, 
-                    sys_hbonds.positions, sys_hbonds.unitcell, 
+            if (el_i in electronegative_elements) & (el_j in electronegative_elements)
+                push_hbond!(hbonds,
+                    i, j, x, y, polar_bonds,
+                    sys_hbonds.positions, sys_hbonds.unitcell,
                     angle_cutoff, ats_sel, d2
                 )
-                push_hbond!(hbonds, 
-                    j, i, y, x, polar_bonds, 
-                    sys_hbonds.positions, sys_hbonds.unitcell, 
+                push_hbond!(hbonds,
+                    j, i, y, x, polar_bonds,
+                    sys_hbonds.positions, sys_hbonds.unitcell,
                     angle_cutoff, ats_sel, d2
                 )
             end
@@ -267,7 +267,7 @@ function hydrogen_bonds(
 end
 
 function push_hbond2!(hbonds,
-    i, j, x, y, polar_bonds, positions, 
+    i, j, x, y, polar_bonds, positions,
     unitcell, ang, ats_sel1, ats_sel2, d2
 )
     # Find if i is a donnor
@@ -293,8 +293,8 @@ end
 
 function hydrogen_bonds(
     ats::AbstractVector{<:PDBTools.Atom},
-    sel1::Union{Function, String},
-    sel2::Union{Function, String};
+    sel1::Union{Function,String},
+    sel2::Union{Function,String};
     donnor_acceptor_distance::Real=3.5f0,
     angle_cutoff::Real=30,
     electronegative_elements=("N", "O", "F", "S"),
@@ -313,8 +313,8 @@ function hydrogen_bonds(
     #
     polar_bonds1 = find_hbond_donnors(
         ats_sel1;
-        positions=ats_sel1_positions, 
-        unitcell, 
+        positions=ats_sel1_positions,
+        unitcell,
         cutoff=donnor_acceptor_distance,
         parallel,
         electronegative_elements,
@@ -343,7 +343,7 @@ function hydrogen_bonds(
         output_name=:hbonds
     )
     map_pairwise!(
-        (x,y,i,j,d2,hbonds) -> begin
+        (x, y, i, j, d2, hbonds) -> begin
             at_i = ats_sel1[i]
             at_j = ats_sel2[j]
             if index(at_i) == index(at_j)
@@ -354,13 +354,13 @@ function hydrogen_bonds(
             end
             el_i = element(at_i)
             el_j = element(at_j)
-            if (el_i in electronegative_elements) & (el_j in electronegative_elements) 
+            if (el_i in electronegative_elements) & (el_j in electronegative_elements)
                 push_hbond2!(hbonds,
-                    i, j, x, y, polar_bonds1, sys_hbonds.xpositions, 
+                    i, j, x, y, polar_bonds1, sys_hbonds.xpositions,
                     unitcell, angle_cutoff, ats_sel1, ats_sel2, d2
                 )
-                push_hbond2!(hbonds, 
-                    j, i, y, x, polar_bonds2, sys_hbonds.ypositions, 
+                push_hbond2!(hbonds,
+                    j, i, y, x, polar_bonds2, sys_hbonds.ypositions,
                     unitcell, angle_cutoff, ats_sel2, ats_sel1, d2
                 )
             end
@@ -374,7 +374,7 @@ end
 @testitem "Hydrogen Bonds" begin
     using PDBTools
     using ShowMethodTesting
-    pdb = read_pdb(PDBTools.test_dir*"/hbonds.pdb")
+    pdb = read_pdb(PDBTools.test_dir * "/hbonds.pdb")
     models = collect(eachmodel(pdb))
 
     #
@@ -385,15 +385,15 @@ end
         @test length(hydrogen_bonds(m, "protein")) == nhb[i]
         @test length(hydrogen_bonds(m, "protein"; parallel=true)) == nhb[i]
     end
-    uc = read_unitcell(PDBTools.test_dir*"/hbonds.pdb")
+    uc = read_unitcell(PDBTools.test_dir * "/hbonds.pdb")
     @test length(hydrogen_bonds(models[1], "protein"; unitcell=uc)) == 63
 
     pbcs = [
-        [ 88.749199, 90.714630, 94.511879],
-        [ 88.838280, 90.805679, 94.606750],
-        [ 88.764839, 90.730606, 94.528542],
-        [ 88.883881, 90.852287, 94.655304],
-        [ 88.828445, 90.795631, 94.596283],
+        [88.749199, 90.714630, 94.511879],
+        [88.838280, 90.805679, 94.606750],
+        [88.764839, 90.730606, 94.528542],
+        [88.883881, 90.852287, 94.655304],
+        [88.828445, 90.795631, 94.596283],
     ]
     nhb = [17964, 17915, 17945, 17977, 17852] # checked with gmx hbond
     for (i, m) in enumerate(models)
@@ -425,7 +425,7 @@ end
             - r is the distance between Donnor and Acceptor atoms (D-A)
             - ang is the angle (degrees) between H-D and A-D.
         """
-    
+
     # Test set overlap error
     @test_throws ArgumentError parse_show(hydrogen_bonds(models[1], "protein", "protein"; unitcell=pbcs[1]))
 
