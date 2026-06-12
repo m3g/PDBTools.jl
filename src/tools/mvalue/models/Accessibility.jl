64a8d0313be728d6a4c006144e8443849c6f8736
lmiq <lmartine@unicamp.br>
2026-06-12 12:59:59 -0300

    testing fits

--- src/tools/mvalue/models/Accessibility.jl
+++ src/tools/mvalue/models/Accessibility.jl
@@ -39,7 +39,7 @@ const cosolvent_column_Accessibility = OrderedDict(
 #
 # Database: All CATH 20 domains, as downloaded in 26/05/2026
 #
-const f_acc = OrderedDict{String, OrderedDict{String, Float32}}(
+_f_acc = OrderedDict{String, OrderedDict{String, Float32}}(
     "ALA" => OrderedDict("n"=>179233, "sc"=>70.878, "sc_pure"=>135.348, "bb"=>49.0121, "bb_pure"=>88.8883, "f_bb"=>0.55139, "f_sc"=>0.523674),
     "PHE" => OrderedDict("n"=>91323, "sc"=>187.587, "sc_pure"=>248.851, "bb"=>38.6632, "bb_pure"=>87.374, "f_bb"=>0.442502, "f_sc"=>0.753811),
     "LEU" => OrderedDict("n"=>213010, "sc"=>159.14, "sc_pure"=>219.486, "bb"=>37.3229, "bb_pure"=>88.2639, "f_bb"=>0.422855, "f_sc"=>0.725056),
@@ -61,6 +61,7 @@ const f_acc = OrderedDict{String, OrderedDict{String, Float32}}(
     "ARG" => OrderedDict("n"=>110038, "sc"=>210.225, "sc_pure"=>272.22, "bb"=>41.319, "bb_pure"=>88.3584, "f_bb"=>0.467629, "f_sc"=>0.772262),
     "CYS" => OrderedDict("n"=>29556, "sc"=>92.4815, "sc_pure"=>155.731, "bb"=>44.936, "bb_pure"=>87.0777, "f_bb"=>0.516044, "f_sc"=>0.593854),
 )
+f_acc() = _f_acc
 
 #
 # Backbone accessibility parameter - [-Infty,1]
@@ -80,26 +81,26 @@ const acc = Dict{String,Float32}(
     "sorbitol" => 0.0,
     "sucrose" => 0.0,
     "trehalose" => 0.0,
-    "urea" => 1.0,
+    "urea" => 0.0,
 )
 
 function model_combination_rule(::Type{Accessibility}, cosolvent, restype)
     cosolvent = lowercase(cosolvent)
     col = cosolvent_column_Accessibility[cosolvent]
     # united model: all bb ASA contributions are the same
-    bb_contribution = GTFEapp["BB"][col] / f_acc["GLY"]["bb_pure"]
+    bb_contribution = GTFEapp["BB"][col] / f_acc()["GLY"]["bb_pure"]
     sc_contribution = if restype == "GLY"
         0.0f0
     else
-        f_sc = f_acc[restype]["f_sc"] # accessibility of the side-chain
-        f_bb = f_acc[restype]["f_bb"] # accessibility of the backbone
+        f_sc = f_acc()[restype]["f_sc"] # accessibility of the side-chain
+        f_bb = f_acc()[restype]["f_bb"] # accessibility of the backbone
         _acc = f_bb^(1 - acc[cosolvent]) # f_bb (protectant) or 1 (urea)
         GTFE_sc = GTFEapp[restype][col] # apparent free energy of the backbone
         tfe_bb = GTFEapp["BB"][col] # apparent free energy of the backbone
         γAA = γ[restype][col] # amino-acid activity correction (usually ignored)
         γG = γ["GLY"][col] # gly-activitity correction - affects all side-chains
         TFE_sc = (1/f_sc) * (GTFE_sc + γAA - γG + tfe_bb * (1 - _acc))
-        TFE_sc / f_acc[restype]["sc_pure"]
+        TFE_sc / f_acc()[restype]["sc_pure"]
     end
     return bb_contribution, sc_contribution
 end
